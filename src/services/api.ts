86c1250b2799f64929d53584fc1ed0a5ee3c3f9f
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2024-08-03 22:32:45 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios.defaults.withCredentials = true;
 const api = axios.create({
-  baseURL: 'https://userbudbackend.onrender.com/',
+  baseURL: 'https://userbudbackend.onrender.com',
 });
 
 export default api;
